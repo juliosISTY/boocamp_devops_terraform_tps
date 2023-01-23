2e0094fbb3c1abe6b1231d0ca85ed98876b52301
Jules Lando <jules-modeste.jeuatsa-lando@ens.uvsq.fr>
2023-01-23 19:50:23 +0100

    TP3 Dynamics deployments

---
+++ tp-3/ec2.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "aws_access_key"
+  secret_key = "aws_secret_key"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "ec2_vm" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.ins_type
+  key_name      = "devops-jules"
+  tags          = var.tag_name
+
+  security_groups = ["${aws_security_group.allow_http_https.id}"]
+
+  root_block_device {
+    delete_on_termination = true
+  }
+}
+
+resource "aws_security_group" "allow_http_https" {
+  name        = "jules-sg"
+  description = "Allow HTTP and HTTPS inbound traffic"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.ec2_vm.id
+  vpc      = true
+}
+
---
+++ tp-3/terraform.tfvars
@@ -0,0 +1,6 @@
+ins_type = "t2.micro"
+
+tag_name = {
+  Name = "ec2-jules2"
+}
+
